1129ceea0bd718b31b4e47fdb9a5455a078cb1bc
WooByeongWook <wbo1026@gmail.com>
2021-01-20 19:27:42 +0900

    회원 도메인(순수 자바코드 Spring X)

--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -1,2 +1,20 @@
-package hello.core;public class MemberApp {
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //jUnit을 이용해서 테스트를 진행하기 전에 자바코드로 확인
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = " + findMember.getName());
+    }
 }
--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements  MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //둘다의존해서 DIP를 위배한다고 판단.
 
     @Override
     public void join(Member member) {

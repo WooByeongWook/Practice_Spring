ee39aa41e740338c95f6b6ebff28c1aee824fe6a
WooByeongWook <wbo1026@gmail.com>
2021-01-21 16:38:41 +0900

    주문과 할인 도메인 실행과 테스트(새로운 할인 정책이 들어오기전)

--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,6 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements  MemberService{
+public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository(); //둘다의존해서 DIP를 위배한다고 판단.
 
